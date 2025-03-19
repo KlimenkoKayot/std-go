606211cc27c11c3fa7c8b2203f9c6a36f8c8e10f
Klimenko George <george_klimenko6@mail.ru>
2025-03-19 15:14:46 +0300

    19.05.2025
    Реализован Once

---
+++ sync/once/once.go
@@ -0,0 +1,48 @@
+//go:build !solution
+
+package once
+
+// Once describes an object that will perform exactly one action.
+type Once struct {
+	wait chan interface{}
+	work chan interface{}
+}
+
+// New creates Once.
+func New() *Once {
+	work := make(chan interface{}, 1)
+	work <- struct{}{}
+	wait := make(chan interface{}, 1)
+	wait <- struct{}{}
+	return &Once{wait, work}
+}
+
+// Do calls the function f if and only if Do is being called for the
+// first time for this instance of Once. In other words, given
+//
+//	once := New()
+//
+// if once.Do(f) is called multiple times, only the first call will invoke f,
+// even if f has a different value in each invocation. A new instance of
+// Once is required for each function to execute.
+//
+// Do is intended for initialization that must be run exactly once.
+//
+// Because no call to Do returns until the one call to f returns, if f causes
+// Do to be called, it will deadlock.
+//
+// If f panics, Do considers it to have returned; future calls of Do return
+// without calling f.
+func (o *Once) Do(f func()) {
+	select {
+	case <-o.work:
+		defer func() {
+			<-o.wait
+		}()
+		f()
+	default:
+		o.wait <- struct{}{}
+		<-o.wait
+		return
+	}
+}
